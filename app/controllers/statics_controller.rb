314ee331ea09e26ff794d8884c5d37fa5d1b636d
ahuang2 <ah698@cornell.edu>
2016-05-07 17:29:33 +0000

    Load homepage champions from API and not database

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -4,18 +4,24 @@ class StaticsController < ApplicationController
   def home
     @roles_hash = Hash.new { |h, k| h[k] = [] }
     
+    url = "http://ddragon.leagueoflegends.com/cdn/#{@current_version}/data/en_US/champion.json"
+    response = HTTParty.get(url)
+    champions = response.parsed_response
+
     # Store all champion name IDs and names---used to generate images and links.
-    Champion.all.each do |champ|
-      champ_name = champ.name # Champion name, e.g. Kha'Zix, Master Yi
-      champ_name_id = champ.champ_name_id # Champion name ID, e.g. KhaZix, MasterYi
+    champions['data'].each do |champ, info|
+      champ_name = info['name'] # Champion name, e.g. Kha'Zix, Master Yi
+      champ_name_id = info['id'] # Champion name ID, e.g. KhaZix, MasterYi
+      primary_role = info['tags'][0] # e.g. "Fighter", "Mage"
       
       # Store champions in their respective roles---used to enable filtering.
       @roles_hash[:All] << [champ_name, champ_name_id]
-      @roles_hash["#{Champion.find_by(champ_name_id: champ_name_id).primary}".to_sym] << [champ_name, champ_name_id]
-      # Sort champions by name.
-      @roles_hash.each do |key, array|
-        @roles_hash[key] = @roles_hash[key].sort
-      end
+      @roles_hash["#{primary_role}".to_sym] << [champ_name, champ_name_id]
+    end
+    
+    # Sort champions by name.
+    @roles_hash.each do |role, champ_list|
+      @roles_hash[role] = champ_list.sort
     end
   end
   
