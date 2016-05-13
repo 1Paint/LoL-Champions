42a88a8408acfe9ec402177c05d46513a390f843
ahuang2 <ah698@cornell.edu>
2016-05-13 06:44:59 +0000

    Add titles to pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def full_title(page_title = "")
+    base_title = "League of Legends Champions"
+    
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
+  
 end
